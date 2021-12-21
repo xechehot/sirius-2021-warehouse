0e907353e77cd94e49e4721ae6f6998d469b934d
asotov <xechehot@gmail.com>
2021-12-21 15:07:41 +0300

    добавил класс для проверки расстояния

---
+++ Maksim/check/dist.py
@@ -0,0 +1,40 @@
+class DistChecker(object):
+    def __init__(self, block_x, block_y, rows, sections):
+        self.block_x = block_x
+        self.block_y = block_y
+        self.rows = rows
+        self.sections = sections
+
+    def dist_to_start(self, place):
+        ans = 0
+        dy = int(abs(self.block_y // 2 + 0.5 - place['block_y']) + 0.5) - 1
+        ans += dy * (self.rows + 2)  # расстояние по горизонтали к центру
+        ans += (self.block_x - place['block_x']) * (self.sections + 2)  # расстояние по вертикали к низу
+        if (place['block_y'] <= self.block_y // 2):
+            ans += (self.rows - place['row'])  # расстояние внутри блока
+        else:
+            ans += place['row']
+        ans += (self.sections + 1 - place['section'])
+        return ans
+
+    def dist_to_tape(self, place):
+        ans = place['section'] + (place['block_x'] - 1) * (self.sections)
+        return ans
+
+    def get_dist_2_places(self, p1, p2):
+        ans = 0
+        if (p1['block_x'] > p2['block_x']): p1, p2 = p2, p1
+        ans += (p2['block_x'] - p1['block_x']) * (self.sections + 2)
+        ans += min(p1['section'] + p2['section'] + 2, 2 * self.sections - p1['section'] - p2['section'] + 2)
+        if (p1['block_y'] > p2['block_y']): p1, p2 = p2, p1
+        ans += (self.rows - p1['row']) + p2['row']
+        ans += (p2['block_y'] - p1['block_y'] - 1) * (self.rows + 2) + 2
+        return ans
+
+    def get_normal_dist(self, path):
+        ans = 0
+        ans += self.dist_to_start(path[0][0]) + self.dist_to_start(path[-1][0])
+        ans += self.dist_to_tape(path[-1][0]) * 2
+        for i in range(len(path) - 1):
+            ans += self.get_dist_2_places(path[i][0], path[i + 1][0])
+        return ans
---
+++ Maksim/check/test_dist.py
@@ -0,0 +1,62 @@
+import pytest
+
+from check.dist import DistChecker
+
+
+def test_simple_sku_block():
+    dist_checker = DistChecker(block_x=1,
+                               block_y=2,
+                               rows=2,
+                               sections=1)
+    path = [
+        (dict(
+            block_x=1,
+            block_y=1,
+            row=1,
+            section=1
+        ),)
+    ]
+    ans = dist_checker.get_normal_dist(path)
+    assert ans == 6
+
+
+def test_one_sku_block():
+    dist_checker = DistChecker(block_x=1,
+                               block_y=2,
+                               rows=20,
+                               sections=10)
+    path = [
+        (dict(
+            block_x=1,
+            block_y=1,
+            row=10,
+            section=2
+        ),)
+    ]
+    ans = dist_checker.get_normal_dist(path)
+    assert ans == 42
+
+
+def test_two_sku_block():
+    dist_checker = DistChecker(block_x=1,
+                               block_y=2,
+                               rows=20,
+                               sections=10)
+    path = [
+        (dict(
+            block_x=1,
+            block_y=1,
+            row=10,
+            section=9
+        ),),
+        (dict(
+            block_x=1,
+            block_y=1,
+            row=10,
+            section=2
+        ),),
+    ]
+    dist_2_point = dist_checker.get_dist_2_places(path[0][0], path[1][0])
+    assert dist_2_point == 7
+    ans = dist_checker.get_normal_dist(path)
+    assert ans == 42
